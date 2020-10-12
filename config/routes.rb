b9173f9051ec24efa7f56ec3eae9d5e506b9c049
Masato Kato <masato.kato0604@gmail.com>
2020-10-12 23:39:44 +0900

    create login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # root routing
@@ -6,4 +7,9 @@ Rails.application.routes.draw do
 
   # user
   resources :users
+
+  # session
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
